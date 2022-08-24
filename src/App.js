cdaa82ed8de07ec23a8dcd2aa87106d05f79fe70
PEDRO <cg3011411@LAB204M30.IFSP>
2022-08-23 21:41:17 -0300

    API configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Tmdb from "./Tmdb";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
 
-export default App;
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+      setMovieList(list);
+
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+      console.log(chosenInfo);
+      setFeatureData(chosenInfo);
+    };
+
+    loadAll();
+  }, []);
+
+  return <div className="page"></div>;
+};
